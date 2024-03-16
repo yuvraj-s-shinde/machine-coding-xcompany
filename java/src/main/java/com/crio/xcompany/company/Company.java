ebb12bb8e9f331d5427a9e4b57be2fe3ef194d4f
CrioUser <crio-user@criodo.com>
2024-03-16 15:47:37 +0530

    ME_BUILDOUT_XCOMPANY_MODULE_XCOMPANY

--- java/src/main/java/com/crio/xcompany/company/Company.java
+++ java/src/main/java/com/crio/xcompany/company/Company.java
@@ -1,5 +1,8 @@
 package com.crio.xcompany.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,7 +10,7 @@ import java.util.Map;
 public class Company{
     private String companyName;
     private Employee founder;
-    private Map<String,Employee> employeeBook;
+    private static Map<String,Employee> employeeBook;
     
 
     private Company(String companyName, Employee founder) {
@@ -30,5 +33,101 @@ public class Company{
     // TODO: CRIO_TASK_MODULE_XCOMPANY
     // Please define all the methods required here as mentioned in the XCompany BuildOut Milestone for each functionality before implementing the logic.
     // This will ensure that the project can be compiled successfully.
+
+    public void registerEmployee(String employeeName, Gender gender) {
+        Employee e = new Employee(employeeName, gender);
+        employeeBook.put(employeeName, e);
+    }
+
+    public Employee getEmployee(String employeeName) {
+        return employeeBook.get(employeeName);
+    }
+
+    public void deleteEmployee(String employeeName) {
+        employeeBook.remove(employeeName);
+    }
+
+    public void assignManager(String employeeName, String managerName) {
+        Employee e = employeeBook.get(employeeName);
+        Employee manager = employeeBook.get(managerName);
+        e.assignManager(manager);
+        employeeBook.put(employeeName, e);
+    }
+
+    public static List<Employee> getDirectReports(String managerName) {
+        List<Employee> res = new ArrayList<>();
+        
+        for (Map.Entry<String,Employee> entry: employeeBook.entrySet()) {
+            if (entry.getValue().getManagerName() != null && entry.getValue().getManagerName().equals(managerName)) {
+                res.add(entry.getValue());
+            }
+        }
+        Collections.sort(res, new Comparator<Employee>() {
+            public int compare(Employee e1, Employee e2) {
+                return e1.getName().compareTo(e2.getName());
+            }
+        });
+        return res;
+    }
+
+    public static List<Employee> getTeamMates(String employeeName) {
+        Employee e = employeeBook.get(employeeName);
+        List<Employee> res = new ArrayList<>();
+        
+        if(e.getManagerName() != null) {
+            Employee mngr = employeeBook.get(e.getManagerName());
+            res.add(mngr);
+        }
+        
+        
+        for (Map.Entry<String,Employee> entry: employeeBook.entrySet()) {
+            if (entry.getValue().getManagerName() != null && entry.getValue().getManagerName().equals(e.getManagerName())) {
+                res.add(entry.getValue());
+            }
+        }
+        Collections.sort(res, new Comparator<Employee>() {
+            public int compare(Employee e1, Employee e2) {
+                return e1.getName().compareTo(e2.getName());
+            }
+        });
+        return res;
+    }
+
+    public List<List<Employee>> getEmployeeHierarchy(String managerName) {
+        Employee e = employeeBook.get(managerName);
+        List<List<Employee>> res = new ArrayList<>();
+        List<Employee> currMgrs = new ArrayList<>();
+        
+        currMgrs.add(e);
+        res.add(currMgrs);
+        List<Employee> nextMgrs = new ArrayList<>();
+        
+        do {
+            nextMgrs = new ArrayList<>();
+            // System.out.println("=======PRINTING curr mgrs========" + currMgrs.toString());
+            for(int i = 0; i < currMgrs.size(); i++) {
+                for (Map.Entry<String,Employee> entry: employeeBook.entrySet()) {
+                    if (entry.getValue().getManagerName() != null && entry.getValue().getManagerName().equals(currMgrs.get(i).getName())) {
+                        nextMgrs.add(entry.getValue());
+                    }
+                }
+            }
+            if (nextMgrs.size() > 0) {
+                Collections.sort(nextMgrs, new Comparator<Employee>() {
+                    public int compare(Employee e1, Employee e2) {
+                        return e1.getName().compareTo(e2.getName());
+                    }
+                });
+
+                res.add(nextMgrs);
+                currMgrs = nextMgrs;
+                // System.out.println("=======PRINTING upcoming curr mgrs========" + currMgrs.toString());
+            } else {
+                break;
+            }
+        } while(true);
+        
+        return res;
+    }
 
 }
--- java/src/main/java/com/crio/xcompany/company/Employee.java
+++ java/src/main/java/com/crio/xcompany/company/Employee.java
@@ -1,11 +1,15 @@
 package com.crio.xcompany.company;
 
+import java.util.List;
 
 public class Employee {
     private String name;
     private Gender gender;
+    private String managerName;
 
     public Employee(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
     }
 
     public String getName() {
@@ -16,9 +20,29 @@ public class Employee {
         return gender;
     }
 
+    public String getManagerName() {
+        return managerName;
+    }
+
     // TODO: CRIO_TASK_MODULE_XCOMPANY
     // Please define all the methods required here as mentioned in the XCompany BuildOut Milestone before implementing the logic.
     // This will ensure that the project can be compiled successfully.
+
+
+    
+    public void assignManager(Employee employee) {
+        this.managerName = employee.getName();
+    }
+
+    public List<Employee> getDirectReports() {
+        return Company.getDirectReports(name);
+    }
+
+    public List<Employee> getTeamMates() {
+        return Company.getTeamMates(name);
+    }
+
+
 
 
     @Override
